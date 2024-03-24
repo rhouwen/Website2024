edb363dec8dc14983f1eac3c5a03e8e60ce81a3b
rhouwen <r.houwen@st.hanze.nl>
2024-03-24 14:22:34 +0100

    Requirements Inloggen

--- forms.py
+++ forms.py
@@ -4,3 +4,23 @@ from wtforms.validators import DataRequired, Email, EqualTo
 from wtforms import ValidationError
 from models import User
 
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm',    message='Wachtwoorden moeten overeenkomen!')])
+    pass_confirm = PasswordField('Confirm password', validators=[DataRequired()])
+    submit = SubmitField('Leg vast!')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('Dit e-mailadres is reeds geregistreerd')
+    
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Deze gebruikersnaam is al in gebruik, probeer een ander naam!')
